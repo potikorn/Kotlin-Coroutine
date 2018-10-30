db038f29ac02cbe838e1b27b6bdd576f476af66e
Potikorn <p.thammo@20scoops.net>
2018-10-30 17:01:28 +0700

    added request http via coroutine with simple retrofit (no RX)

---
+++ app/src/main/java/com/example/potikorn/kotlincoroutine/httpmanager/HttpManager.kt
@@ -0,0 +1,22 @@
+package com.example.potikorn.kotlincoroutine
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object HttpManager {
+
+    fun getRetrofitInstance() =
+        Retrofit.Builder()
+            .baseUrl("https://randomuser.me/api/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(
+                OkHttpClient.Builder().addInterceptor(
+                    HttpLoggingInterceptor().setLevel(
+                        HttpLoggingInterceptor.Level.BODY
+                    )
+                ).build()
+            )
+            .build()
+}
